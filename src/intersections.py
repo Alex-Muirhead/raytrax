a713645a90660e42b47fa3214ac4eb5af7065bbe
Alex Muirhead <a.muirhead@uq.net.au>
2026-01-15 17:31:28 +1000

    Implement crossing & handle some edge cases

--- src/intersections.py
+++ src/intersections.py
@@ -1,21 +1,55 @@
 import equinox as eqx
 import jax.numpy as jnp
 import numpy as np
-from jaxtyping import Array, Float
+from jaxtyping import Array, Float, Int
+from equinox._misc import default_floating_dtype
 
 
 class ConvexCell(eqx.Module):
-    normals: Float[Array, "ncells nfaces ndim"]
-    offsets: Float[Array, "ncells nfaces"]
+    normal: Float[Array, "... nfaces ndim"]
+    offset: Float[Array, "... nfaces"]
 
     @eqx.filter_jit
-    def contains(self, points: Float[Array, "npoints ndim"], epsilon: float = 0.0):
+    def contains(self, points: Float[Array, "... ndim"], epsilon: float = 0.0):
         if epsilon == 0.0:
-            epsilon = np.finfo(points.dtype).resolution
-        point_offsets = jnp.einsum("...k,...jk->...j", points, self.normals)
-        return jnp.all(point_offsets <= self.offsets + epsilon, axis=-1)
+            epsilon = np.finfo(default_floating_dtype()).resolution
+        # Signed distance "outside" each half-space.
+        distance = jnp.einsum("...k,...jk->...j", points, self.normal) - self.offset
+        return jnp.all(distance <= epsilon, axis=-1)
 
 
 class LinearRay(eqx.Module):
-    origin: Float[Array, "nrays ndim"]
-    tangent: Float[Array, "nrays ndim"]
+    terminus: Float[Array, "... ndim"]
+    tangent: Float[Array, "... ndim"]
+    travel: Float[Array, "..."]
+
+    def __check_init__(self):
+        if self.terminus.shape != self.tangent.shape:
+            raise ValueError("Shapes must match!")
+
+
+# @eqx.filter_jit
+def crossing(
+    cell: ConvexCell, ray: LinearRay, epsilon: float = 0.0
+) -> tuple[Int[Array, "..."], Float[Array, "..."]]:
+    if epsilon == 0.0:
+        # We probably shouldn't use zero, due to floating precision / loss
+        epsilon = np.finfo(default_floating_dtype()).resolution
+
+    # Signed distance "outside" each half-space.
+    absolute_distance = jnp.einsum("...k,...jk->...j", ray.terminus, cell.normal) - cell.offset
+    alignment = jnp.einsum("...k,...jk->...j", ray.tangent, cell.normal)
+    relative_distance = absolute_distance + alignment * ray.travel
+    absolute_travel: Float[Array, "... nfaces"] = -absolute_distance / alignment
+    # Take minimum value greater than current travel
+    absolute_travel = jnp.where(alignment > epsilon, absolute_travel, jnp.nan)
+    print(relative_distance, relative_distance > epsilon)
+    crossing_index: Int[Array, "..."] = jnp.where(
+        jnp.any(relative_distance > epsilon, axis=-1),
+        jnp.nanargmax(relative_distance, axis=-1),
+        jnp.nanargmin(absolute_travel, axis=-1),
+    )
+    crossing_travel: Float[Array, "..."] = jnp.maximum(
+        absolute_travel[..., crossing_index] - ray.travel, 0
+    )
+    return crossing_index, crossing_travel
--- test/test_intersections.py
+++ test/test_intersections.py
@@ -1,29 +1,49 @@
 import numpy as np
-from intersections import ConvexCell
+import pytest
+
+from intersections import ConvexCell, LinearRay, crossing
 
 
-def unit_2D_cell() -> ConvexCell:
-    """Generate a unit-square cell."""
+def unit_vec(a: list[float] | np.NDArray):
+    vec = np.asarray(a)
+    return vec / np.linalg.norm(vec)
+
+
+def unit_2D_cell(x: int = 0, y: int = 0) -> ConvexCell:
+    """Generate a unit-square cell.
+
+    Square axis-alignend cell with unit side lengths.
+    Can be offset.
+        1
+      +---+
+    2 |   | 0
+      +---+
+        3
+
+    Params:
+        x (int): The lower-left x-coordinate
+        y (int): The lower-left y-coordinate
+    """
     # fmt: off
-    normals = np.array(
+    normal = np.array(
         [[+1.0,  0.0],
          [ 0.0, +1.0],
          [-1.0,  0.0],
          [ 0.0, -1.0]],
         dtype=float
     )
-    offsets = np.array(
-        [1.0, 1.0, 0.0, 0.0],
+    offset = np.array(
+        [x + 1, y + 1, x, y],
         dtype=float
     )
     # fmt: on
-    return ConvexCell(normals=normals, offsets=offsets)
+    return ConvexCell(normal=normal, offset=offset)
 
 
 def unit_3D_cell() -> ConvexCell:
     """Generate a unit-cube cell."""
     # fmt: off
-    normals = np.array(
+    normal = np.array(
         [[+1.0,  0.0,  0.0],
          [ 0.0, +1.0,  0.0],
          [ 0.0,  0.0, +1.0],
@@ -32,12 +52,12 @@ def unit_3D_cell() -> ConvexCell:
          [ 0.0,  0.0, -1.0]],
         dtype=float
     )
-    offsets = np.array(
+    offset = np.array(
         [1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
         dtype=float
     )
     # fmt: on
-    return ConvexCell(normals=normals, offsets=offsets)
+    return ConvexCell(normal=normal, offset=offset)
 
 
 def random_2D_quad() -> ConvexCell:
@@ -57,7 +77,7 @@ def random_2D_quad() -> ConvexCell:
         dtype=int
     )
     # fmt: on
-    normals = ...
+    normal = ...
     raise NotImplementedError()
 
 
@@ -136,3 +156,62 @@ def test_3D_containment_edges():
     expected = np.array([True, True, True, True])
 
     assert np.all(actual == expected)
+
+
+testdata = [
+    (np.array([+1.0, 0.0]), 0, 0.5),
+    (np.array([0.0, +1.0]), 1, 0.5),
+    (np.array([-1.0, 0.0]), 2, 0.5),
+    (np.array([0.0, -1.0]), 3, 0.5),
+]
+
+
+@pytest.mark.parametrize("direction,expected_face,expected_travel", testdata)
+def test_2D_crossing_from_center(direction, expected_face, expected_travel):
+    ray = LinearRay(
+        terminus=np.array([0.5, 0.5]),
+        tangent=direction,
+        travel=0.0,  # Does this need to be an array too?
+    )
+    cell = unit_2D_cell()
+
+    actual_face, actual_travel = crossing(cell, ray)
+    assert actual_face == expected_face
+    assert actual_travel == pytest.approx(expected_travel)
+
+
+def test_2D_crossing_poor_alignment():
+    # Start inside the cell, on the facet
+    # We point *just* across the facet
+    ray = LinearRay(
+        terminus=np.array([0.0, 0.5]),  # West facet, index=2
+        tangent=unit_vec([-1e-7, 1.0]),
+        travel=0.0,
+    )
+    # Our alignment is small (1e-10 for testing)
+    cell = unit_2D_cell()
+
+    # We expect to skip the problematic west boundary
+    face, travel = crossing(cell, ray, epsilon=1e-6)
+    assert face == 1
+    assert travel == pytest.approx(0.5)
+
+
+def test_2D_crossing_poor_precondition():
+    # A follow-up to the previous test
+    # We crossed the north facet (1)
+    ray = LinearRay(
+        terminus=np.array([0.0, 0.5]),  # West facet, index=2
+        tangent=unit_vec([-1e-7, 1.0]),
+        travel=0.5,
+    )
+    # We now sit left of the south facet (3) of this next cell
+    cell = unit_2D_cell(x=0, y=1)
+
+    # Normally, this would take several dozen cells to correct,
+    # however we artificially control the epsilon to see the desired
+    # result, that the path is correct by crossing the west boundary.
+    face, travel = crossing(cell, ray, epsilon=1e-12)
+    assert face == 2
+    # Hmm... this travel assumes that the boundaries are a regular grid
+    assert travel == 0.0
